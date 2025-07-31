edac5b6f171d3ff19239ee811d6cad3b5e57ec3d
Nihal <nihalmuhaednihal@gmail.com>
2025-07-31 14:29:05 +0530

    Initial chat app commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const users = {};
+
+io.on("connection", socket => {
+    socket.on("new-user", name => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-connected", name);
+    });
+
+    socket.on("send-chat-message", message => {
+        socket.broadcast.emit("chat-message", {
+            message: message,
+            name: users[socket.id]
+        });
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user-disconnected", users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
