d2a79629fcdb222867a270f2b436e14129a46387
Nihal <nihalmuhaednihal@gmail.com>
2025-07-31 19:01:23 +0530

    Initial chat app commit

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socket = io("http://localhost:3000");
+    const socket = io("https://noob-chat-1.onrender.com");
 
     const messageForm = document.getElementById("send-container");
     const messageInput = document.getElementById("message-input");
